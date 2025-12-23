cb760819f43aeef1760c9f866b5c000500aaaed6
Angel LeGrande <angellegrande14209@gmail.com>
2025-12-23 05:18:06 -0800

    commit

--- src/main/java/org/yearup/WheelsRecordsApplication.java
+++ src/main/java/org/yearup/WheelsRecordsApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WheelsRecordsApplication
+public class  WheelsRecordsApplication
 {
 
     public static void main(String[] args) {
